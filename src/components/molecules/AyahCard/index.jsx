5c5e1d2f552c6a108ef1e5c0c2b285e61c277d63
ahmadrivaldys <ahmadrivaldys@gmail.com>
2021-07-27 18:41:32 +0700

    Styling surah detail and add audio playback for murrotal

--- src/components/molecules/AyahCard/index.jsx
+++ src/components/molecules/AyahCard/index.jsx
@@ -3,11 +3,33 @@ import './index.css'
 
 const AyahCard = (props) =>
 {
-    console.log(props)
+    // console.log(props)
+    // const [ayahAudio, setAyahAudio] = useState(null)
+
+    const sn = props.surahNumber
+    const an = props.ayah.number.inSurah
+    const audioId   = `surah-${sn}-audio-${an}`
+    const buttonId  = `audio-button-${sn}-${an}`
+    const playIcon  = `play-${sn}-${an}`
+    const pauseIcon = `pause-${sn}-${an}`
+    const srcAudio  = props.ayah.audio.primary
+
     return (
         <div className="ayah-card">
             <div className="ayah-toolbar">
-                <div className="ayah-number">{props.ayah.number.inSurah}</div>
+                <div className="ayah-number">{an}</div>
+                
+                <div className="audio-wrapper">
+                    <audio id={audioId} src={srcAudio} />
+                    <button id={buttonId} className="audio-button" onClick={() => props.getPlay(audioId, buttonId, playIcon, pauseIcon)} data-play="false">
+                        <svg aria-hidden="true" id={playIcon} focusable="false" data-prefix="fas" data-icon="play" className="svg-inline--fa fa-play fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                            <path d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z"></path>
+                        </svg>
+                        <svg aria-hidden="true" id={pauseIcon} focusable="false" data-prefix="fas" data-icon="pause" className="svg-inline--fa fa-pause fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">'
+                            <path d="M144 479H48c-26.5 0-48-21.5-48-48V79c0-26.5 21.5-48 48-48h96c26.5 0 48 21.5 48 48v352c0 26.5-21.5 48-48 48zm304-48V79c0-26.5-21.5-48-48-48h-96c-26.5 0-48 21.5-48 48v352c0 26.5 21.5 48 48 48h96c26.5 0 48-21.5 48-48z"></path>'
+                        </svg>
+                    </button>
+                </div>
             </div>
 
             <p className="ayah-ar">{props.ayah.text.arab}</p>
--- src/pages/Surah/index.jsx
+++ src/pages/Surah/index.jsx
@@ -1,11 +1,26 @@
 import React, { Component } from 'react'
 import { AyahCard } from '../../components'
 import QuranAPI from '../../services'
+import './index.css'
 
-export class Surah extends Component
+class Surah extends Component
 {
     state =
     {
+        name:
+        {
+            arabic: '',
+            transliteration: '',
+            translation: ''
+        },
+        surahNumber: 0,
+        numberOfAyahs: 0,
+        revelation: '',
+        preBismillah:
+        {
+            text: '',
+            audio: ''
+        },
         ayahList: []
     }
 
@@ -14,12 +29,71 @@ export class Surah extends Component
         let id = this.props.match.params.id
 
         QuranAPI.getSurah(id).then(result =>
-        {
-            // console.log('Ini suratnya: ', result.data)
+        {            
+            let data              = result.data
+            let preBismillah      = data.preBismillah == null ? '' : data.preBismillah.text.arab
+            let preBismillahAudio = data.preBismillah == null ? '' : data.preBismillah.audio.primary
+
             this.setState({
-                ayahList: result.data.verses
+                name:
+                {
+                    arabic: data.name.long,
+                    transliteration: data.name.transliteration.id,
+                    translation: data.name.translation.id
+                },
+                surahNumber: data.number,
+                numberOfAyahs: data.numberOfVerses,
+                revelation: data.revelation.id,
+                preBismillah:
+                {
+                    text: preBismillah,
+                    audio: preBismillahAudio
+                },
+                ayahList: data.verses
             })
         })
+    }
+
+    playAudio = (getAudio, getButtonDataPlay, getPlayIcon, getPauseIcon) =>
+    {
+        const theAudio  = document.getElementById(getAudio)
+        const btn       = document.getElementById(getButtonDataPlay)
+        const playIcon  = document.getElementById(getPlayIcon)
+        const pauseIcon = document.getElementById(getPauseIcon)
+        const dataPlay  = btn.getAttribute('data-play')
+
+        if(dataPlay === 'false')
+        {
+            btn.setAttribute('data-play', 'true')
+            const playingAudio = theAudio.play()
+
+            if(playingAudio !== undefined)
+            {
+                playingAudio.then(() =>
+                {
+                    playIcon.style.display = 'none'
+                    pauseIcon.style.display = 'block'
+                }).catch((error) => 
+                {
+                    console.log(error)
+                })
+            }
+        }
+        else
+        {
+            btn.setAttribute('data-play', 'false')
+            theAudio.pause()
+            playIcon.style.display = 'block'
+            pauseIcon.style.display = 'none'
+        }
+
+        theAudio.onpause = () =>
+        {
+            btn.setAttribute('data-play', 'false')
+            theAudio.pause()
+            playIcon.style.display = 'block'
+            pauseIcon.style.display = 'none'
+        }
     }
     
     componentDidMount()
@@ -29,21 +103,42 @@ export class Surah extends Component
     
     render()
     {
+        const surah = this.state
+
         return (
             <>
-                <div className="header-background">
-                    <div className="header-illustration">
-                        <div className="header-text">
-                            <h1>Daftar Surat dalam Al-Qur'an</h1>
-                            <p>Nama Surat</p>
-                        </div>
+                <div className="surah-header-background">
+                    <div className="surah-header-illustration"></div>
+                    <div className="surah-header-text">
+                        <p className="arabic-name">{surah.name.arabic}</p>
+                        <h1>{surah.name.transliteration}</h1>
+                        <p>{surah.name.translation}</p>
+                        <hr />
+                        <h5>{surah.revelation} &middot; {surah.numberOfAyahs} Ayat</h5>
+                        {
+                            surah.preBismillah.text !== ''
+                            ?
+                                <div className="pre-bismillah-wrapper">
+                                    <audio id="pre-bismillah-audio" src={surah.preBismillah.audio} />
+                                    <button id="btn-playback" className="pre-bismillah-btn-play" onClick={() => this.playAudio('pre-bismillah-audio', 'btn-playback', 'fa-play', 'fa-pause')} data-play="false">
+                                        <svg aria-hidden="true" id="fa-play" focusable="false" data-prefix="fas" data-icon="play" className="svg-inline--fa fa-play fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                            <path d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z"></path>
+                                        </svg>
+                                        <svg aria-hidden="true" id="fa-pause" focusable="false" data-prefix="fas" data-icon="pause" className="svg-inline--fa fa-pause fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">'
+                                            <path d="M144 479H48c-26.5 0-48-21.5-48-48V79c0-26.5 21.5-48 48-48h96c26.5 0 48 21.5 48 48v352c0 26.5-21.5 48-48 48zm304-48V79c0-26.5-21.5-48-48-48h-96c-26.5 0-48 21.5-48 48v352c0 26.5 21.5 48 48 48h96c26.5 0 48-21.5 48-48z"></path>'
+                                        </svg>
+                                    </button>
+                                    <p className="pre-bismillah">{surah.preBismillah.text}</p>
+                                </div>
+                            : ''
+                        }
                     </div>
                 </div>
                 <div className="content content-bg">
                     {
-                        this.state.ayahList.map(ayah =>
+                        surah.ayahList.map(ayah =>
                         {
-                            return <AyahCard key={ayah.number.inSurah} ayah={ayah} />
+                            return <AyahCard key={ayah.number.inSurah} surahNumber={surah.surahNumber} ayah={ayah} getPlay={this.playAudio} />
                         })
                     }
                 </div>
