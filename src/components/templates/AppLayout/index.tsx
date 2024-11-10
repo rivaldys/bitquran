4e0f540d70cf00ce53fa0216caa831ac5eb2c3b1
rivaldys <ahmadrivaldys@gmail.com>
2024-11-10 22:53:17 +0700

    chore: update meta data

--- src/components/templates/AppLayout/index.tsx
+++ src/components/templates/AppLayout/index.tsx
@@ -13,14 +13,15 @@ export default function AppLayout({ children }: AppLayoutProps)
                 <meta name="keywords" content="Bitquran, Al-Qur'an Digital, Al-Qur'an Online, Al-Qur'an Daring, Al-Qur'an Indonesia" />
                 <meta name="twitter:title" content="Bitquran &#8211; Baca Al-Qur'an secara Daring" />
                 <meta name="twitter:description" content="Website yang menyajikan kitab suci Al-Qur'an secara digital. Dilengkapi terjemahan, tafsir, dan audio ayat (murottal)." />
-                <meta name="twitter:image" content="/cover-image.png" />
+                <meta name="twitter:image" content={`${process.env.NEXT_PUBLIC_URL}/cover-image.png`} />
                 <meta name="twitter:card" content="summary_large_image" />
                 <meta property="og:title" content="Bitquran &#8211; Baca Al-Qur'an secara Daring" />
                 <meta property="og:description" content="Website yang menyajikan kitab suci Al-Qur'an secara digital. Dilengkapi terjemahan, tafsir, dan audio ayat (murottal)." />
                 <meta property="og:url" content={process.env.NEXT_PUBLIC_URL} />
-                <meta property="og:image" content="/cover-image.png" />
+                <meta property="og:image" content={`${process.env.NEXT_PUBLIC_URL}/cover-image.png`} />
                 <meta property="og:image:width" content="1200" />
                 <meta property="og:image:height" content="630" />
+                <meta property="og:site_name" content="Bitquran" />
             </Head>
 
             <Header />
