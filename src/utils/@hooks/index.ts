9cab9401a76e0798b45946be8eea1500d4b8264e
ahmad.sudrajat <ahmad.sudrajat@petrosea.com>
2025-01-16 09:42:52 +0700

    refactor: move out some utils functions from `src\utils` into `src\utils\@hooks`

---
+++ src/utils/@hooks/index.ts
@@ -0,0 +1,3 @@
+export * from './useEventListener'
+export * from './useForm'
+export * from './useNetworkStatus'
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,3 @@
+export * from './@hooks'
+export * from './@types'
 export * from './browserMind'
-export * from './useEventListener'
-export * from './useForm'
