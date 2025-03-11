d3588a42dbe96161e3404f00a0284c2e2a995bcd
ahmad.sudrajat <ahmad.sudrajat@petrosea.com>
2025-03-11 10:04:09 +0700

    refactor: change error handle when fetching API in main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,26 @@
-import { SurahProps } from 'bitquran/types'
+import { SurahsProps } from 'bitquran/types'
 import Surah from './surat'
 
 export async function getServerSideProps()
 {
     try {
         const res = await fetch(`${process.env.API_URL}/surah`)
-        if(!res.ok) throw new Error('Failed to fetch data')
+        if(!res.ok) throw new Error('Failed to fetch surah list.')
 
         const surahs = await res.json()
 
         return { props: { surahs } }
     }
-    catch(error: any) {
-        return { props: { error: error.message } }
+    catch(error: unknown) {
+        return {
+            props: {
+                error: error instanceof Error ? error.message : 'An unknown error occurred',
+            }
+        }
     }
 }
 
-export default function Main({ surahs }: SurahProps)
+export default function Main({ surahs }: SurahsProps)
 {
     return <Surah surahs={surahs} />
 }
