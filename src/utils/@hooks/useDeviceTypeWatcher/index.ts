6ace4ed00b8a349008fc3393f383fd435e8e84f5
rivaldys <ahmadrivaldys@gmail.com>
2025-04-20 10:16:29 +0700

    fix: hydration

--- src/utils/@hooks/useDeviceTypeWatcher/index.ts
+++ src/utils/@hooks/useDeviceTypeWatcher/index.ts
@@ -1,13 +1,14 @@
 import { useMemo } from 'react'
 import useWindowDimensions from '../useWindowDimensions'
 
-const useDeviceTypeWatcher = () =>
+const useDeviceTypeWatcher = (): 'desktop' | 'mobile' =>
 {
     const { windowWidth } = useWindowDimensions()
     const mobileModeMinWidth = 768
 
     const deviceType = useMemo(() =>
     {
+        if(!windowWidth) return 'desktop'
         return windowWidth > mobileModeMinWidth ? 'desktop' : 'mobile'
     }, [windowWidth, mobileModeMinWidth])
 
