d59ca9049d6f7d878918ac4581a8153d343c0315
rivaldys <ahmadrivaldys@gmail.com>
2025-04-25 16:45:52 +0700

    refactor: remove unused component

--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -1,12 +1,10 @@
 import Footer from './Footer'
-import Header from './Header'
 import NavigationBar from './NavigationBar'
 import SurahItem from './SurahItem'
 import VerseItem from './VerseItem'
 
 export {
     Footer,
-    Header,
     NavigationBar,
     SurahItem,
     VerseItem
