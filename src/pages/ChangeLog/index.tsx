ceea57960e2c2b1fd88f5094124a23d113512b0d
rivaldys <ahmadrivaldys@gmail.com>
2025-06-29 12:18:08 +0700

    feat(ui): add new pages: `About`, `ChangeLog`, `Surah`

---
+++ src/pages/ChangeLog/index.tsx
@@ -0,0 +1,54 @@
+import { ContentSection, PageContentLayout, SectionTitle, TextLabel } from 'bitquran/components'
+import { Head } from 'bitquran/shared/lib'
+
+export default function ChangeLog()
+{
+    return (
+        <>
+            <Head>
+                <title>Riwayat Pembaruan &#8211; Bitquran</title>
+            </Head>
+
+            <PageContentLayout>
+                <ContentSection>
+                    <SectionTitle>Riwayat Pembaruan</SectionTitle>
+                    <TextLabel>Versi 1.2.0</TextLabel>
+                    <ul className="pl-[30px] list-disc">
+                        <li>Pemutakhiran teknologi utama situs ini dari React v17 ke React v19 (Vite) dan beberapa teknologi pendukung lainnya.</li>
+                    </ul>
+                </ContentSection>
+
+                <ContentSection>
+                    <TextLabel>Versi 1.1.2</TextLabel>
+                    <ul className="pl-[30px] list-disc">
+                        <li>Menambahkan konfigurasi <i>base url/public url</i>.</li>
+                    </ul>
+                </ContentSection>
+
+                <ContentSection>
+                    <TextLabel>Versi 1.1.1</TextLabel>
+                    <ul className="pl-[30px] list-disc">
+                        <li>Mengubah <i>endpoint API</i>.</li>
+                        <li>Menambahkan gambar pratinjau (<i>preview image</i>) ketika <i>link</i> situs ini dibagikan di media sosial.</li>
+                        <li>Tahun <i>copyright</i> dinamis sesuai tahun berjalan.</li>
+                    </ul>
+                </ContentSection>
+
+                <ContentSection>
+                    <TextLabel>Versi 1.1.0</TextLabel>
+                    <ul className="pl-[30px] list-disc">
+                        <li>Menubar pada versi <i>mobile</i> dipindahkan menjadi <i>sidebar menu</i> yang dapat dimunculkan dengan mengklik tombol menu yang ada di sudut kanan layar.</li>
+                        <li>Menambahkan fitur pencarian surat pada laman beranda.</li>
+                    </ul>
+                </ContentSection>
+
+                <ContentSection className="!border-b-0">
+                    <TextLabel>Versi 1.0.0</TextLabel>
+                    <ul className="pl-[30px] list-disc">
+                        <li>Rilis awal</li>
+                    </ul>
+                </ContentSection>
+            </PageContentLayout>
+        </>
+    )
+}
---
+++ src/pages/Surah/index.tsx
@@ -0,0 +1,37 @@
+import { Input, SurahItem } from 'bitquran/components'
+import { ILQuran } from 'bitquran/images'
+import type { SurahsProps } from 'bitquran/shared/types'
+
+export default function Surah({ surahs }: SurahsProps)
+{
+    return (
+        <>
+            <div className="rounded-[7px] sm:rounded-[10px] [background-image:linear-gradient(150deg,_#c5e1a5,_#009688)] mb-[20px] sm:mb-[30px]">
+                <div
+                    className="flex items-center h-[150px] sm:h-[250px] [background-position:bottom_-25px_right_-10px] sm:[background-position:bottom_-50px_right_-20px] bg-no-repeat [background-size:35%] sm:[background-size:30%]"
+                    style={{ backgroundImage: `url(${ILQuran})` }}
+                >
+                    <div className="ml-[25px] sm:ml-[50px] box-border [&>*]:text-white">
+                        <h1 className="text-[22px] leading-[29px] sm:text-[36px] sm:leading-[43px] font-semibold">Daftar Surat dalam Al-Qur'an</h1>
+                        <p className="text-[14px] leading-[21px] sm:text-[20px] sm:leading-[27px]">114 Surat · Makkiyah · Madaniyah</p>
+                    </div>
+                </div>
+            </div>
+
+            <Input className="mb-[20px] md:mb-[30px]" placeholder="Cari nama surat" />
+
+            {surahs &&
+                surahs.data.map(surah => (
+                    <SurahItem
+                        containerClassName="mb-[15px]"
+                        number={surah.number}
+                        title={surah.name.transliteration.id}
+                        description={surah.name.translation.id}
+                        label={surah.name.short}
+                        href={`/surat/${surah.number}`}
+                        key={`surah-${surah.number}`}
+                    />
+                ))}
+        </>
+    )
+}
